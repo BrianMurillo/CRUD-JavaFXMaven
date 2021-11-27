f930007fe1b9109bad6f861ba797db61f8bc951f
Brian Murillo <bmurillo1800@innovaccion.mx>
2021-11-27 01:10:39 -0600

    changes finalized

--- src/main/java/com/upiicsa/crudjavafxmaven/model/Conexion.java
+++ src/main/java/com/upiicsa/crudjavafxmaven/model/Conexion.java
@@ -5,11 +5,9 @@
  */
 package com.upiicsa.crudjavafxmaven.model;
 
-import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 import javafx.scene.control.Alert;
 
 /**
